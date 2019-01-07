d8595783240f3fcb7deb2a669433f5ce281c0b80
Logan Wright <logan.william.wright@gmail.com>
2019-01-07 16:22:36 -0600

    injection formatting cleanup

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
         .package(url: #(dependency.gitUrl), from: #(dependency.version)),
     }],
     targets: [
-        .target(name: "App", dependencies: [#for(dependency in dependencies) {#for(include in dependency.includes) {"#(include)",
+        .target(name: "App", dependencies: [
+            #for(dependency in dependencies) {#for(include in dependency.includes) {"#(include)",
             }}]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
