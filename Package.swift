a93781afe06118022c361f256714374ec2c86fdf
Logan Wright <logan.william.wright@gmail.com>
2019-01-07 16:15:46 -0600

    better formatting

--- Package.swift
+++ Package.swift
@@ -3,17 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "VaporApp",
-    dependencies: [
-        #for(dependency in dependencies) {
-            // #(dependency.comment)
-            #(dependency.package),
-        }
-    ],
+    dependencies: [#for(dependency in dependencies) {
+        // #(dependency.comment)
+        #(dependency.package),
+    }],
     targets: [
-        .target(name: "App", dependencies: [
-            #for(dependency in dependencies) {#for(include in dependency.includes) {"#(include)",
-            }}
-        ]),
+        .target(name: "App", dependencies: [#for(dependency in dependencies) {#for(include in dependency.includes) {"#(include)",
+            }}]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
