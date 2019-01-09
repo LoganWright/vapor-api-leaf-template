36b7388b2354cf41f4221c2a8869005b2a56f200
Logan Wright <logan.william.wright@gmail.com>
2019-01-09 16:57:13 -0600

    update to go back to more templating focus

--- Package.swift
+++ Package.swift
@@ -5,10 +5,18 @@ let package = Package(
     name: "#(packageName)",
     dependencies: [
         // vapor web framework
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),   
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        #if(fluent.include) {
+            .package(url: "https://github.com/vapor/fluent-#lowercased(fluent.db.name).git", from: "#(fluent.db.version")
+        }
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "App", dependencies: [
+            "Vapor",
+            #if(fluent.include) {
+                "Fluent#(fluent.db.name)"
+            }
+        ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
